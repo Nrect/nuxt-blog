e077b35475cb2f14bd28ce4b52085fe8ac70f7b2
Aleksandr L <bossstartov57@gmail.com>
2020-12-10 22:46:09 +0200

    Post card and post detail layouts

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-container>
+    <el-header>
+      <navigation/>
+    </el-header>
+    <el-main>
+      <Nuxt/>
+    </el-main>
+    <el-footer/>
+  </el-container>
+</template>
+
+<script>
+import Navigation from "~/components/main/Navigation";
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+
+</style>
