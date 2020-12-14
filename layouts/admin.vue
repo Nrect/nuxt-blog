dc77676801da07e2c0fbbe3d5daea9ace3288420
Aleksandr L <bossstartov57@gmail.com>
2020-12-14 22:36:17 +0200

    Admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="admin-layout-wrap">
+    <el-container :style="{height:'100%'}">
+      <el-aside>
+        Aside
+      </el-aside>
+      <el-main>
+        <nuxt/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "admin"
+}
+</script>
+
+<style scoped>
+.admin-layout-wrap {
+  width: 100%;
+  height: 100vh;
+}
+</style>
