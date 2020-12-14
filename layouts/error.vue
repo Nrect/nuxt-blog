3b9bc325a80f1722e3c6243260ef2c70f53d646f
Aleksandr L <bossstartov57@gmail.com>
2020-12-14 20:56:46 +0200

    Error layout

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="error-content">
+    <div class="error-content__content">
+      <h1 class="error-content__title">Страница не найдена!</h1>
+      <nuxt-link to="/">Вернуться на главную</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error"
+}
+</script>
+
+<style lang="scss" scoped>
+.error-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.error-content__content {
+  display: flex;
+  flex-direction: column;
+  padding-top: 4rem;
+  justify-content: center;
+}
+
+.error-content__title {
+  color: #ff0048;
+}
+</style>
