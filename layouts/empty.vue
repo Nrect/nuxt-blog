9e88fe961f79d144b1e371bc68caaf6b2e80f369
Aleksandr L <bossstartov57@gmail.com>
2020-12-15 17:03:18 +0200

    Admin login page

---
+++ layouts/empty.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-layout">
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.empty-layout {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 4rem;
+}
+</style>
