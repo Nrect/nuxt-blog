342817cb5b52243bcd6c436851315dee7c25386d
Aleksandr L <bossstartov57@gmail.com>
2020-12-16 23:56:19 +0200

    Errors

--- store/auth.js
+++ store/auth.js
@@ -13,10 +13,16 @@ export const mutations = {
 
 export const actions = {
   async login({commit, dispatch}, formData) {
-    const token = await new Promise(resolve => {
-      setTimeout(() => resolve('mock-token'), 2000)
-    })
-    dispatch('setToken', token)
+    try {
+      const token = await new Promise((resolve, reject) => {
+        setTimeout(() => reject('mock-token'), 2000)
+      })
+      dispatch('setToken', token)
+    } catch (e) {
+      // {root: true} будем искать в корне
+      commit('setError', e, {root: true})
+      throw e
+    }
   },
   setToken({commit}, token) {
     commit('setToken', token)
