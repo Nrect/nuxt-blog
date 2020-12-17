eb53e0eac1d7abdee1380f70bd95beed294260ca
Aleksandr L <bossstartov57@gmail.com>
2020-12-17 21:57:29 +0200

    List posts

---
+++ store/post.js
@@ -0,0 +1,14 @@
+export const actions = {
+  async fetchAdmin({}) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          {title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: Math.random()},
+          {title: 'Post 2', date: new Date(), views: 32, comments: [1, 2], _id: Math.random()},
+          {title: 'Post 3', date: new Date(), views: 65, comments: [1, 2], _id: Math.random()},
+
+        ])
+      }, 1200)
+    })
+  }
+}
